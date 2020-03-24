40490e38ac66e954433544fb098f913ee579b1aa
dk457 <61482924+dk457@users.noreply.github.com>
2020-03-24 10:45:27 +0900

    Mar 24
    After office hour

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("JS Loaded!");
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Navigation
+    nav.classList.toggle("nav-active");
+
+    //Animation Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 1 ease forwards ${index / 2 +
+          1.5}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
